2924473eee74c12b96f7d9035d75cfbd8b75e14e
Seb <sebastien@cubecom.fr>
2020-12-30 16:07:52 +0100

    llimite of screen

--- main.py
+++ main.py
@@ -27,11 +27,13 @@ while running:
     
     # verifier si le joueur souhaite aller de gauche à droite 
     # print(game.pressed)
-    if game.pressed.get(pygame.K_RIGHT):
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
         game.player.move_right()
         
-    elif game.pressed.get(pygame.K_LEFT):
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
         game.player.move_left()
+        
+    print(game.player.rect.x)
     
     #mettre à jour l'écran
     pygame.display.flip()
