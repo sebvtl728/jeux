17b3c1bbe0dbb886d66cd719221d9ab141e722d6
Seb <sebastien@cubecom.fr>
2021-01-08 12:09:58 +0100

    speed monster MAJ

--- monster.py
+++ monster.py
@@ -15,8 +15,11 @@ class Monster(animation.AnimateSprite):
         self.rect = self.image.get_rect()
         self.rect.x = 1000 + random.randint(0, 300)
         self.rect.y = 540 - offset
-        self.velocity = random.randint(1, 3)
         self.start_animation()
+        
+    def set_speed(self, speed):
+        self.default_speed = speed
+        self.velocity = random.randint(1, self.default_speed)
         
     def damage(self, amount):
         #infliger les degats
@@ -60,6 +63,7 @@ class Mechant(Monster):
     
     def __init__(self, game):
         super().__init__(game, 'mummy', (130, 130))
+        self.set_speed(3)
         
 #definir une class pour le super monstre
 class SuperMechant(Monster):
@@ -68,3 +72,5 @@ class SuperMechant(Monster):
         super().__init__(game, 'alien',(300, 300), 130)
         self.health = 250
         self.max_health = 250
+        self.attack = 0.8
+        self.set_speed(1)
